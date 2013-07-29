eb1faa5ef54e9c8442cdb23a163aa5c0aece22e3
ethanlim <mail@ethanlim.net>
2013-07-30 02:42:37 +0800

    Added a command line shell for the ccmf library

--- shell/main.js
+++ shell/main.js
@@ -1,6 +1,7 @@
 /* Required Modules*/
 var ccmf = require('ccmf'),
-	fs = require('fs');
+	fs = require('fs'),
+	firebase = require('firebase');
 
 /* Open the file */
 
@@ -12,18 +13,32 @@ fs.readFile('/Users/ethanlim/Downloads/reuters/reut2-000.sgm',function read(err,
 function process(content){
 	var textContent = content.toString(),
 		bodyTextArr = null,
+		registeringText = '',
 		bodyIdx = 0,
 		max = 0,
-		pattern = /<DATE>(.*?)<\/DATE>/;
 	
-	bodyTextArr = textContent.match(/<\s*BODY[^>]*>([^<]*)<\s*\/\s*BODY\s*>/g);
-	
+	bodyTextArr  = textContent.match(/<\s*BODY[^>]*>([^<]*)<\s*\/\s*BODY\s*>/g);
 	
 	if(bodyTextArr!==null){
 		for(bodyIdx=0,max=bodyTextArr.length;bodyIdx<max;bodyIdx++){
-			console.log(bodyIdx);
-			console.log(bodyTextArr[bodyIdx].replace(/(<([^>]+)>)/ig,""));
+			registeringText = bodyTextArr[bodyIdx].replace(/(<([^>]+)>)/ig,"");
+			
+			var textMod = ccmf.ccmf.Text.create();
+			var dataMod = ccmf.ccmf.Data.create();
+			
+			var registeringTextShingles = textMod.removedStopWordShingles(registeringText,9);
+			
+			var registerShinglesFing = textMod.shinglesFingerprintConv(registeringTextShingles);
+			
+			/* Extract the Signature */
+			var signature = [];
+			signature[0] = registerShinglesFing; 
+			var minHashSignature = textMod.minHashSignaturesGen(signature);
+			
+			/* Register the Signature into the Registry*/
+			dataMod.storeLsh(minHashSignature);
 		}
+		
 	}else{
 		console.log("No String Found");
 	}
