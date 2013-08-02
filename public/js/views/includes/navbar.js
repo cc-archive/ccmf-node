852015f6487b6c556e0d0ec67ca576fb65987d1b
ethanlim <mail@ethanlim.net>
2013-08-03 00:01:24 +0800

    change the navbar javascript

--- public/js/views/includes/navbar.js
+++ public/js/views/includes/navbar.js
@@ -4,17 +4,19 @@ jQuery(document).ready(function(){
 	
 	var currentLoc = currentLocArr[1];
 	
-	jQuery('.navbar-items a').each(function(index) {
-		
-		/* Get the controller name */
-		var linkLocArr = this.href.split("/");
-		
-		var linkLoc = linkLocArr[3];
-		
-		if(linkLoc == currentLoc){
-            jQuery(this).closest('li.menu-item').addClass("active");
-		}else{
-			jQuery(this).closest('li.menu-item').removeClass("active");
-		}
-	});
+	if(currentLoc!==""){
+		jQuery('.navbar-items a').each(function(index) {
+			
+			/* Get the controller name */
+			var linkLocArr = this.href.split("/");
+			
+			var linkLoc = linkLocArr[3];
+			
+			if(linkLoc == currentLoc){
+	            jQuery(this).closest('li.menu-item').addClass("active");
+			}else{
+				jQuery(this).closest('li.menu-item').removeClass("active");
+			}
+		});
+	}
 });
