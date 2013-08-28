9866b70b3db64aa64965d2dd78bdd547c765d5cc
ethanlim <mail@ethanlim.net>
2013-08-28 10:26:23 +0800

    Added the backend for the installation page

--- routes/infos.js
+++ routes/infos.js
@@ -1,6 +1,11 @@
 /* Information Route */
 
 module.exports = function(app){
+	
+	app.get('/infos/installation',function(req,res){
+		  res.render('infos/installation', { title: 'Installation' });
+	});
+	
 	app.get('/infos/searchs',function(req,res){
 		  res.render('infos/searchs', { title: 'Search Result' });
 	});
